5c7b58cd010cfbd10285ce6b7f5f12e06960a603
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-01 05:06:25 -0300

    create project

---
+++ back-end/src/test/java/br/com/dental_care/DentalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dental_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
