f7cbe393b48713698ecfbd8a154f1ea7d1c30aa0
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-13 06:33:45 -0300

    feat: create user and role entities

---
+++ back-end/src/main/java/br/com/dental_care/model/Role.java
@@ -0,0 +1,53 @@
+package br.com.dental_care.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity(name = "tb_role")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String authority;
+
+  @ManyToMany(mappedBy = "roles")
+  private final Set<User> users = new HashSet<>();
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Role other = (Role) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+}
