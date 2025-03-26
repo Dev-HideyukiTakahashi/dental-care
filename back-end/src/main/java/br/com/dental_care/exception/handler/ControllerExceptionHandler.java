7d7b35286465fefe4baaf28b8d99d444e7919c51
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-26 04:24:48 -0300

    refactor: ControllerExceptionHandler

--- back-end/src/main/java/br/com/dental_care/exception/handler/ControllerExceptionHandler.java
+++ back-end/src/main/java/br/com/dental_care/exception/handler/ControllerExceptionHandler.java
@@ -1,8 +1,9 @@
 package br.com.dental_care.exception.handler;
 
-import java.time.Instant;
-
+import br.com.dental_care.exception.DatabaseException;
+import br.com.dental_care.exception.ResourceNotFoundException;
 import br.com.dental_care.exception.ScheduleConflictException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +13,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import br.com.dental_care.exception.DatabaseException;
-import br.com.dental_care.exception.ResourceNotFoundException;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<CustomError> handleNotFound(ResourceNotFoundException e, WebRequest request){
+  public ResponseEntity<CustomError> handleNotFound(ResourceNotFoundException e, HttpServletRequest request){
     
     CustomError customError = CustomError
               .builder()
@@ -27,28 +27,28 @@ public class ControllerExceptionHandler {
               .status(HttpStatus.NOT_FOUND.value())
               .error(e.getMessage())
               .message("Not Found Error")
-              .path(request.getDescription(false))
+              .path(request.getRequestURI())
               .build();
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(customError);
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ValidationError> handleValidation(MethodArgumentNotValidException e, 
-                                                                WebRequest request){
+  public ResponseEntity<ValidationError> handleValidation(MethodArgumentNotValidException e,
+                                                          HttpServletRequest request){
     ValidationError customError = new ValidationError(
                       Instant.now(), 
                       HttpStatus.UNPROCESSABLE_ENTITY.value(), 
                       e.getDetailMessageCode(), 
                       "Validation Error in Fields",
-                      request.getDescription(false));
+                      request.getRequestURI());
     e.getFieldErrors().forEach(error -> customError.addError(error));
     
     return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(customError);
   }
 
   @ExceptionHandler(DatabaseException.class)
-  public ResponseEntity<CustomError> handleDatabase(DatabaseException e, WebRequest request){
+  public ResponseEntity<CustomError> handleDatabase(DatabaseException e, HttpServletRequest request){
     
     CustomError customError = CustomError
               .builder()
@@ -56,7 +56,7 @@ public class ControllerExceptionHandler {
               .status(HttpStatus.CONFLICT.value())
               .error(e.getMessage())
               .message("Conflict Error")
-              .path(request.getDescription(false))
+              .path(request.getRequestURI())
               .build();
 
     return ResponseEntity.status(HttpStatus.CONFLICT).body(customError);
@@ -64,14 +64,14 @@ public class ControllerExceptionHandler {
 
   @ExceptionHandler(InvalidDataAccessApiUsageException.class)
   public ResponseEntity<CustomError> handleInvalidDataAccessApiUsage(InvalidDataAccessApiUsageException e,
-                                                                                WebRequest request){
+                                                                     HttpServletRequest request){
     CustomError customError = CustomError
               .builder()
               .timestamp(Instant.now())
               .status(HttpStatus.BAD_REQUEST.value())
               .error(e.getMessage())
               .message("Bad Request")
-              .path(request.getDescription(false))
+              .path(request.getRequestURI())
               .build();
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customError);
@@ -79,14 +79,14 @@ public class ControllerExceptionHandler {
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<CustomError> handleHttpMessageNotReadable(HttpMessageNotReadableException e,
-                                                                     WebRequest request){
+                                                                  HttpServletRequest request){
     CustomError customError = CustomError
             .builder()
             .timestamp(Instant.now())
             .status(HttpStatus.BAD_REQUEST.value())
             .error(e.getMessage())
             .message("Bad Request")
-            .path(request.getDescription(false))
+            .path(request.getRequestURI())
             .build();
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customError);
@@ -94,14 +94,14 @@ public class ControllerExceptionHandler {
 
   @ExceptionHandler(ScheduleConflictException.class)
   public ResponseEntity<CustomError> handleHttpMessageNotReadable(ScheduleConflictException e,
-                                                                  WebRequest request){
+                                                                  HttpServletRequest request){
     CustomError customError = CustomError
             .builder()
             .timestamp(Instant.now())
             .status(HttpStatus.CONFLICT.value())
             .error(e.getMessage())
             .message("Conflict in data of schedule")
-            .path(request.getDescription(false))
+            .path(request.getRequestURI())
             .build();
 
     return ResponseEntity.status(HttpStatus.CONFLICT).body(customError);
