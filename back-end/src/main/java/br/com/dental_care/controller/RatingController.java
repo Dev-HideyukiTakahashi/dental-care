470af6defd0c73dd7eead957613011e959ad68d0
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-03 07:00:40 -0300

    fix: add authorization in RatingController

--- back-end/src/main/java/br/com/dental_care/controller/RatingController.java
+++ back-end/src/main/java/br/com/dental_care/controller/RatingController.java
@@ -1,13 +1,21 @@
 package br.com.dental_care.controller;
 
+import br.com.dental_care.dto.AbsenceDTO;
 import br.com.dental_care.dto.RatingDTO;
+import br.com.dental_care.exception.handler.CustomError;
 import br.com.dental_care.service.RatingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +33,35 @@ public class RatingController {
     private final RatingService ratingService;
     private final Logger logger = LoggerFactory.getLogger(RatingController.class);
 
+    @Operation(
+            summary = "Rate a dentist",
+            description = "Submit a rating for a dentist, providing feedback based on the service received.",
+            tags = "Rating")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Rating successfully added.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RatingDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class))),
+            @ApiResponse(responseCode = "403", description = "Forbidden",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class))),
+            @ApiResponse(responseCode = "404", description = "Dentist not found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class))),
+            @ApiResponse(responseCode = "409", description = "Schedule data conflict",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomError.class)))
+    })
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_PATIENT')")
     public ResponseEntity<RatingDTO> rateDentist(@Valid @RequestBody RatingDTO dto) {
         logger.info("Starting rating process for dentist ID: {}", dto.getDentistId());
         dto = ratingService.rateDentist(dto);
