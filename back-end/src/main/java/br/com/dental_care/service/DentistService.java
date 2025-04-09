8c26d31ee8c6be562d740c6a587f4ed908d4c5f6
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-09 01:14:33 -0300

    refactor: dentist update/save validate email/password encode

--- back-end/src/main/java/br/com/dental_care/service/DentistService.java
+++ back-end/src/main/java/br/com/dental_care/service/DentistService.java
@@ -8,8 +8,10 @@ import br.com.dental_care.exception.ResourceNotFoundException;
 import br.com.dental_care.mapper.DentistMapper;
 import br.com.dental_care.mapper.RoleMapper;
 import br.com.dental_care.model.Dentist;
+import br.com.dental_care.model.User;
 import br.com.dental_care.repository.DentistRepository;
 import br.com.dental_care.repository.RoleRepository;
+import br.com.dental_care.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -17,9 +19,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,8 +32,9 @@ public class DentistService {
 
     private final DentistRepository dentistRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
     private final Logger logger = LoggerFactory.getLogger(DentistService.class);
-
 
     @Transactional(readOnly = true)
     public DentistDTO findById(Long id) {
@@ -48,6 +54,9 @@ public class DentistService {
     @Transactional
     public DentistDTO save(DentistDTO dto) {
         Dentist dentist = new Dentist();
+        userRepository.findByEmail(dto.getEmail()).ifPresent(user -> {
+                    throw new DatabaseException("Email already exists.");
+        });
         copyToEntity(dentist, dto);
         dentist = dentistRepository.save(dentist);
         logger.info("New dentist has been created with id: {}", dentist.getId());
@@ -58,30 +67,33 @@ public class DentistService {
     public DentistDTO update(DentistDTO dto, Long id) {
         try {
             Dentist dentist = dentistRepository.getReferenceById(id);
+            validateEmail(dto, dentist.getId());
             copyToEntity(dentist, dto);
             dentist = dentistRepository.save(dentist);
             logger.info("Dentist updated successfully, id: {}", dentist.getId());
             return DentistMapper.toDTO(dentist);
-        } catch (EntityNotFoundException e) {
+        } catch(EntityNotFoundException e) {
             throw new ResourceNotFoundException("Dentist not found! ID: " + id);
+        }catch(DataIntegrityViolationException e) {
+            throw new DatabaseException("Email already exists.");
         }
     }
 
     @Transactional(propagation = Propagation.SUPPORTS)
     public void deleteById(Long id) {
-        if (!dentistRepository.existsById(id))
+        if(!dentistRepository.existsById(id))
             throw new ResourceNotFoundException("Dentist not found! ID: " + id);
         try {
             dentistRepository.deleteById(id);
             logger.info("Dentist deleted, id: {}", id);
-        } catch (DataIntegrityViolationException e) {
+        } catch(DataIntegrityViolationException e) {
             throw new DatabaseException("Database error: Data integrity rules were violated.");
         }
     }
 
     public void copyToEntity(Dentist dentist, DentistDTO dto) {
         dentist.setName(dto.getName());
-        dentist.setPassword(dto.getPassword());
+        dentist.setPassword(passwordEncoder.encode(dto.getPassword()));
         dentist.setEmail(dto.getEmail());
         dentist.setPhone(dto.getPhone());
         dentist.setRegistrationNumber(dto.getRegistrationNumber());
@@ -91,12 +103,18 @@ public class DentistService {
     }
 
     private void checkRoles(Dentist dentist, DentistDTO dto) {
-        for (RoleDTO role : dto.getRoles()) {
-            if (!roleRepository.existsById(role.getId())){
+        for(RoleDTO role : dto.getRoles()) {
+            if(!roleRepository.existsById(role.getId())) {
                 logger.warn("Attempted to create a dentist with a non-existent role");
                 throw new ResourceNotFoundException("Role does not exist, id: " + role.getId());
             }
             dentist.getRoles().add(RoleMapper.toEntity(role));
         }
+    }
+
+    private void validateEmail(DentistDTO dto, Long dentistId) {
+        Optional<User> existingUser = userRepository.findByEmail(dto.getEmail());
+        if(existingUser.isPresent() && !existingUser.get().getId().equals(dentistId))
+            throw new DatabaseException("Email already exists.");
     }
 }
