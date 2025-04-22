1380f342fd8b255da133bd0ad2af9e69070efb23
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-22 04:28:29 -0300

    refactor: LocalDateTime to parse

--- back-end/src/test/java/br/com/dental_care/factory/ScheduleFactory.java
+++ back-end/src/test/java/br/com/dental_care/factory/ScheduleFactory.java
@@ -12,19 +12,19 @@ public class ScheduleFactory {
     public static ScheduleDTO createValidScheduleDTO() {
         return ScheduleDTO.builder()
                 .id(1L)
-                .unavailableTimeSlot(LocalDateTime.of(2027, 12, 20, 8, 0, 0, 0))
-                .absenceStart(LocalDateTime.of(2027, 12, 20, 9, 0, 0, 0))
-                .absenceEnd(LocalDateTime.of(2027, 12, 20, 12, 0, 0, 0))
+                .unavailableTimeSlot(LocalDateTime.parse("2027-12-20T08:00:00"))
+                .absenceStart(LocalDateTime.parse("2027-12-20T10:00:00"))
+                .absenceEnd(LocalDateTime.parse("2027-12-30T10:00:00"))
                 .build();
     }
 
     public static Schedule createValidSchedule() {
         Schedule schedule = new Schedule();
         schedule.setId(1L);
-        schedule.setUnavailableTimeSlot(LocalDateTime.of(2027, 12, 20, 8, 0, 0, 0));
+        schedule.setUnavailableTimeSlot(LocalDateTime.parse("2027-12-20T08:00:00"));
         schedule.setDentist(DentistFactory.createValidDentist());
-        schedule.setAbsenceStart(LocalDateTime.of(2027, 12, 20, 9, 0, 0, 0));
-        schedule.setAbsenceEnd(LocalDateTime.of(2027, 12, 20, 12, 0, 0, 0));
+        schedule.setAbsenceStart(LocalDateTime.parse("2027-12-20T10:00:00"));
+        schedule.setAbsenceEnd(LocalDateTime.parse("2027-12-30T10:00:00"));
 
         return schedule;
     }
