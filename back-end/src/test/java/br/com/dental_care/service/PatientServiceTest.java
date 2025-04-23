4a3741c35b93397d9b55eb21317d62028e5778b4
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-23 07:36:02 -0300

    chore: rename class

--- back-end/src/test/java/br/com/dental_care/service/PatientServiceTest.java
+++ back-end/src/test/java/br/com/dental_care/service/PatientServiceTest.java
@@ -30,7 +30,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class PatientServiceTests {
+public class PatientServiceTest {
 
     @InjectMocks
     private PatientService patientService;
