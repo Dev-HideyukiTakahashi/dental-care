edb0b70d126edffa32526587c8a7715a047488a5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-27 02:57:04 -0300

    refactor: docs ScheduleController to yaml

--- back-end/src/main/java/br/com/dental_care/controller/ScheduleController.java
+++ back-end/src/main/java/br/com/dental_care/controller/ScheduleController.java
@@ -3,15 +3,7 @@ package br.com.dental_care.controller;
 
 import br.com.dental_care.dto.AbsenceDTO;
 import br.com.dental_care.dto.AbsenceRequestDTO;
-import br.com.dental_care.dto.ScheduleDTO;
-import br.com.dental_care.exception.handler.CustomError;
 import br.com.dental_care.service.ScheduleService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -24,40 +16,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/schedules")
 @RequiredArgsConstructor
-@Tag(name = "Schedule", description = "Endpoints for managing schedules")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
     private final Logger logger = LoggerFactory.getLogger(ScheduleController.class);
 
-    @Operation(summary = "Schedule a dentist absence",
-            description = "Create a new absence schedule for the dentist, " +
-                          "marking a period when they will be unavailable to attend appointments.",
-            tags = "Schedule")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Absence successfully " +
-                                                             "created in Schedule.",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = AbsenceDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "403", description = "Forbidden",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "404", description = "Dentist not found",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "409", description = "Schedule data conflict",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class)))
-    })
     @PostMapping("/absences/dentist/{id}")
     @PreAuthorize("hasRole('ROLE_DENTIST')")
     public ResponseEntity<AbsenceDTO> createDentistAbsence
@@ -68,32 +31,6 @@ public class ScheduleController {
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
-    @Operation(summary = "Remove a dentist absence",
-            description = "Cancel a previously scheduled absence for the dentist.",
-            tags = "Schedule")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Schedule successfully deleted.",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ScheduleDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "403", description = "Forbidden",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "404", description = "Schedule not found",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "409", description = "Schedule data conflict",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class)))
-    })
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_DENTIST')")
     public ResponseEntity<Void> removeDentistAbsence(@PathVariable Long id) {
