29c38df7621b7c1f0c5c26b0431d3374fc42ee2d
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-04-27 02:57:12 -0300

    refactor: docs UserController to yaml

--- back-end/src/main/java/br/com/dental_care/controller/UserController.java
+++ back-end/src/main/java/br/com/dental_care/controller/UserController.java
@@ -1,14 +1,7 @@
 package br.com.dental_care.controller;
 
 import br.com.dental_care.dto.PatientDTO;
-import br.com.dental_care.exception.handler.CustomError;
 import br.com.dental_care.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,31 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
-@Tag(name = "User", description = "Endpoints for managing users")
 public class UserController {
 
     private final UserService userService;
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @Operation(summary = "Get logged-in patient", description = "Retrieves the currently authenticated patient.",
-            tags = "Patient")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Patient retrieved successfully",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = PatientDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "403", description = "Forbidden",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error",
-                    content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = CustomError.class)))
-    })
     @PreAuthorize("hasRole('ROLE_PATIENT')")
     @GetMapping(path = "/me")
     public ResponseEntity<PatientDTO> getLoggedUser() {
