f0cba50b7119b94db85c4bf4f7410978c0911d75
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-05-05 05:55:12 -0300

    chore: config locale pt-BR

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 };
