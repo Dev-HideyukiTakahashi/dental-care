b75226b2b607567ccf56b2bb3e23f346b6fe4bec
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-05-06 08:25:22 -0300

    feat: modal in header - logout

--- front-end/src/app/shared/components/header/header.component.ts
+++ front-end/src/app/shared/components/header/header.component.ts
@@ -1,17 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { AuthService } from '../../../core/service/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private authService = inject(AuthService);
+  showLogoutModal = false;
+
   @Output() toggleSidebarEvent = new EventEmitter<void>();
   sidebarCollapsed: boolean = false;
 
   toggleSidebar() {
     this.sidebarCollapsed = !this.sidebarCollapsed;
     this.toggleSidebarEvent.emit();
+  }
+
+  openLogoutModal() {
+    this.showLogoutModal = true;
+  }
+
+  closeModal() {
+    this.showLogoutModal = false;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.closeModal();
   }
 }
