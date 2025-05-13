d4ae19465738b31e039bf4ce23613eb8ed0dba58
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-05-13 00:13:37 -0300

    feat: initial layout dashboard

--- front-end/src/app/features/dashboard/dashboard.component.ts
+++ front-end/src/app/features/dashboard/dashboard.component.ts
@@ -4,8 +4,10 @@ import { Component } from '@angular/core';
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+  totalPacientes = 0;
+  totalConsultas = 0;
+  totalDentistas = 0;
 }
